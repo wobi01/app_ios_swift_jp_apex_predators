868481490de98c6e6ce4326f14045bffbc65e9ad
Wojciech <wbiskup@edu.cdv.pl>
2024-05-28 09:46:09 +0200

    Initial Commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Wobi on 28/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
